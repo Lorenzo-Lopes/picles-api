17854b3457dbf7b10a1491c90378f0d22ae3de52
Lorenzo-Lopes <lorenzolopes.dev@gmail.com>
2024-04-04 17:10:49 -0300

    alteração na validação de entrada.

--- src/shelter/dtos/update.shelter.controller.input.ts
+++ src/shelter/dtos/update.shelter.controller.input.ts
@@ -1,13 +1,16 @@
-import { IsEmail, IsNotEmpty, IsNumberString, IsString, Length } from "class-validator";
+import { IsEmail, IsNotEmpty, IsNumberString, IsOptional, IsString, Length } from "class-validator";
 
 export default class UpdateShelterControlerInput {
+    @IsOptional()	
     @IsNotEmpty()
     @IsString()
     name: string;
+    @IsOptional()	
     @IsNotEmpty()
     @IsString()
     @Length(10,11)
     whatsapp: string;
+    @IsOptional()
     @IsNotEmpty()
     @IsNumberString()
     phone: string;
